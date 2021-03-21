19a637559672e25a16702d6d47648bfffc0ff112
Medina Pilavdzic <medina.pilavdzic@gmail.com>
2021-03-21 13:05:38 +0100

    Added Project for Day13-14

---
+++ ex01/scripts/index.js
@@ -0,0 +1,52 @@
+// Get the modal
+var myImg = ['id']
+id = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+myImg[1];
+myImg[2];
+myImg[3];
+myImg[4];
+myImg[5];
+myImg[6];
+myImg[7];
+myImg[8];
+myImg[9];
+myImg[10];
+
+
+function myImgFunction() {
+    myImg[myImg.id].style.display = "block";
+    if (myImg.style.display === block);
+    return myImg;
+
+    return myImg[id];
+
+};
+
+console.log(myImgFunction('myImg'));
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+
+// }
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
+
+// {
+//     /* <button id="myBtn">Open Modal</button>
+
+
+//     <div id="myModal" class="modal">
+
+//       <div class="modal-content">
+//         <span class="close">&times;</span>
+//         <p>Some text in the Modal..</p>
+//       </div>
+
+//     </div> */
+// }
